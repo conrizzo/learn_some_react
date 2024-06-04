f7f641b00000d281b1b9b3201a8c4b0b16ca9b11
Conrad Lundberg <conrizzo@gmail.com>
2024-06-04 13:47:54 +0200

    some basic updates to React / Next.js project - Focusing on learning more

---
+++ conrad_project/src/components/FirstComponent/FirstComponent.tsx
@@ -0,0 +1,17 @@
+
+import styles from './FirstComponent.module.css';
+
+
+export default function Profile() {
+    return (
+        <div>
+            <h1>Bird Chirp,</h1>
+            <img
+                  src="/images/diffBird.jpg"
+                alt="Bird in the sky"
+            />
+            <figcaption className={styles.centerWithFlex}>Just a picture</figcaption>
+        </div>
+    )
+}
+
