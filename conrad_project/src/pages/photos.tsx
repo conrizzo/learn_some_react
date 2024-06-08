d0e5bac1f8873283e511e62d7001ba4a5ffc7866
Conrad Lundberg <conrizzo@gmail.com>
2024-06-08 11:13:12 +0200

    message to redeploy

--- conrad_project/src/pages/photos.tsx
+++ conrad_project/src/pages/photos.tsx
@@ -2,10 +2,10 @@
 import fs from 'fs';
 import path from 'path';
 import React from 'react';
-import styles from './Photos.module.css';
+import styles from './photos.module.css';
 
 export async function getStaticProps() {
-    
+
     const imagesDirectory = path.join(process.cwd(), 'public/images/Germany');
     const filenames = fs.readdirSync(imagesDirectory);
     const imagePaths = filenames.map(filename => `/images/Germany/${filename}`);
@@ -19,16 +19,15 @@ export async function getStaticProps() {
 
 interface PhotosProps {
     imagePaths: Array<string>;
-  }
-  
-  const Photos: React.FC<PhotosProps> = ({ imagePaths }) => {
-      return (
-          <div className={styles.photosContainer}>
-              {imagePaths.map((path, index) => (
-                  <img key={index} src={path} alt={`Photo ${index + 1}`} className={styles.photo} />
-              ))}
-          </div>
-      );
-  };
+}
+const Photos: React.FC<PhotosProps> = ({ imagePaths }) => {
+    return (
+        <div className={styles.photosContainer}>
+            {imagePaths.map((path, index) => (
+                <img key={index} src={path} alt={`Photo ${index + 1}`} className={styles.photo} />
+            ))}
+        </div>
+    );
+};
 
 export default Photos;
