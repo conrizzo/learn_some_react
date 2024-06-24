fd8d1c3f91b6ed94dfc5fb163d6c187b4908ce27
Conrad Lundberg <conrizzo@gmail.com>
2024-06-24 18:44:16 +0200

    add useEffect and a synonym array on each agree

--- conrad_project/src/pages/agree-to-terms.tsx
+++ conrad_project/src/pages/agree-to-terms.tsx
@@ -1,7 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from '../components/Modals/ConfirmationModal';
 import TermsCheckbox from '../components/Forms/checkbox';
 import styles from './agree-to-terms.module.css';
+
+const synonyms = [
+    "completely",
+    "entirely",
+    "fully",
+    "perfectly",
+    "thoroughly",
+    "wholly",
+    "utterly",
+    "quite", // informal
+    "certainly",
+    "definitely",
+    "of course", // agreement
+    "positively",
+    "surely",
+    "undoubtedly",
+    "unquestionably",
+    "without fail",
+    "totally",
+    "downright",
+    "flat-out", // informal
+    "radically",
+    "stone-cold", // informal
+    "hands down", // informal
+    "absolutely", // used for emphasis
+    "categorically",
+    "indisputably",
+    "indubitably",
+    "veritably", // archaic
+    "plain",
+    "cold", // slang
+    "realistically",
+    "frankly",
+    "truly",
+    "generally",
+    "admittedly",
+    "mostly",
+    "truthfully",
+    "substantially",
+    "largely",
+    "for sure",
+    "you bet",
+    "no doubt",
+    "definitely", // informal
+    "totally", // informal
+    "completely", // informal
+    "for real", // informal
+    "exactly",
+    "indeed",
+    "all together",
+    "assuredly",
+    "in its entirety"
+];
+
 
 // Define a type for the modal object
 type ModalType = {
@@ -9,11 +63,27 @@ type ModalType = {
     index: number;
 };
 
+function pickAndRemoveRandomElement(arr: string[]): string | undefined {
+
+    if (arr.length === 0) {
+        return undefined;
+    }
+
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const [pickedElement] = arr.splice(randomIndex, 1);
+    return pickedElement;
+}
+
+
 const AgreeToTerms: React.FC = () => {
+    const getSynonym = pickAndRemoveRandomElement(synonyms)
+
     const [modals, setModals] = useState<ModalType[]>([{ isOpen: false, index: -1 }]);
     const [checkboxStates, setCheckboxStates] = useState<boolean[]>([false]);
 
-    const generateDynamicText = (index: number) => Array(index).fill('absolutely').join(', ');
+    const generateDynamicText = (index: number) => Array(index).fill(
+        getSynonym
+    ).join(', ');
 
     const handleCheckboxChange = (index: number) => {
         const updatedStates = [...checkboxStates];
@@ -53,21 +123,32 @@ const AgreeToTerms: React.FC = () => {
         setModals(updatedModals);
     };
 
+    const [dynamicText, setDynamicText] = useState('');
+    const index = modals.length - 1; // Define the 'index' variable
+    useEffect(() => {
+        // This code runs when the component mounts and whenever `index` changes
+        setDynamicText(generateDynamicText(index));
+    }, [index]); // Dependency array, `generateDynamicText` is called when `index` changes
+
+
     return (
         <div>
-            <div className={`${styles.centering} ${styles['agree-background']}`}>
 
-            <div className={styles['vertical-container']}>
-                <div className={styles['check-box']}>
-                    <TermsCheckbox
-                        labelText={`Do you agree to the terms and conditions?`}
-                        isChecked={checkboxStates[0]}
-                        onChange={() => handleCheckboxChange(0)}
-                    />
-                </div>
-            
-                    <button className="clean-button" onClick={() => addModal(0)}>Confirm</button>
-               
+
+            <div className={styles.centering}>
+                <div className={`${styles.centering} ${styles['agree-background']}`}>
+
+                    <div className={styles['vertical-container']}>
+                        <div className={styles['check-box']}>
+
+                            <TermsCheckbox
+                                labelText={`Do you agree to the terms and conditions?`}
+                                isChecked={checkboxStates[0]}
+                                onChange={() => handleCheckboxChange(0)}
+                            />
+                        </div>
+                        <button className="clean-button" onClick={() => addModal(0)}>Confirm</button>
+                    </div>
                 </div>
             </div>
             {modals.map((modal, index) => (
@@ -75,11 +156,11 @@ const AgreeToTerms: React.FC = () => {
                     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                         <div className={styles['check-box']}>
                             <TermsCheckbox
-                                labelText={`Are you ${generateDynamicText(index)} sure you agree to the terms and conditions?`}
-                                
+                                labelText={`Are you ${dynamicText} sure you agree to the terms and conditions?`}
+
                                 isChecked={checkboxStates[index]}
                                 onChange={() => handleCheckboxChange(index)}
-                            /> 
+                            />
                             <br></br>
                             <br></br>
                             {'You agreed ' + index + ' times'}
